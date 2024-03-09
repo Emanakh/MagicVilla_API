fc4b4095b83da92a399e8b402f7484828b41e7d5
Eman Alaa Khodary <emanalaakhodary@gmail.com>
2024-03-09 03:31:45 +0200

    villa model, get mthod

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+
+	[ApiController]
+	//[Route("api/[Controller]")]
+	[Route("api/VillaAPI")]
+	public class VillaAPIController : ControllerBase
+	{
+		[HttpGet]
+		public IEnumerable<Villa> GetVillas()
+		{
+			return new List<Villa> {
+				new Villa { Id = 1, Name = "beach" },
+				new Villa { Id = 2, Name = "Honey" }
+			};
+		}
+	}
+}
