4ba4b860d58cf380dd134311b5ba829c3ee98207
Eman Alaa Khodary <emanalaakhodary@gmail.com>
2024-03-11 04:07:11 +0200

    update return type to (action result) and the action return to (status codes) , check id with if coditions

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -12,15 +12,25 @@ namespace MagicVilla_VillaAPI.Controllers
 	public class VillaAPIController : ControllerBase
 	{
 		[HttpGet]
-		public IEnumerable<VillaDTO> GetVillas()
+		public ActionResult<IEnumerable<VillaDTO>> GetVillas()
 		{
-			return VillaStore.VillaList;
+			return Ok(VillaStore.VillaList);
 
 		}
 		[HttpGet("{id:int}")]
-		public VillaDTO GetVilla(int id)
+		public ActionResult<VillaDTO> GetVilla(int id)
 		{
-			return VillaStore.VillaList.FirstOrDefault(s => s.Id == id);
+			if (id == 0)
+			{
+				return BadRequest("");
+			}
+			var villa = VillaStore.VillaList.FirstOrDefault(s => s.Id == id);
+			if (villa == null)
+			{
+				return NotFound();
+			}
+			return Ok(villa);
+
 
 		}
 	}
