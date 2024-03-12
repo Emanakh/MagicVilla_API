56b27b2dc91c764b998676a5097deb5c42167745
Eman Alaa Khodary <emanalaakhodary@gmail.com>
2024-03-13 00:15:48 +0200

    AutoMapper Configuration

---
+++ MagicVilla_VillaAPI/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.DTO;
+
+namespace MagicVilla_VillaAPI
+{
+	public class MappingConfig : Profile
+	{
+		public MappingConfig()
+		{
+			CreateMap<Villa, VillaDTO>().ReverseMap();
+			CreateMap<Villa, VillaCreatedDTO>().ReverseMap();
+			CreateMap<Villa, VillaUpdatedDTO>().ReverseMap();
+
+		}
+	}
+}
--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,3 +1,4 @@
+using MagicVilla_VillaAPI;
 using MagicVilla_VillaAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
@@ -9,6 +10,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(option =>
 {
 	option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
 });
+
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 
 Log.Logger = new LoggerConfiguration().MinimumLevel.Information().WriteTo.File("log/villaLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
 builder.Host.UseSerilog();
