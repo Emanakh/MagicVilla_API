bcf92d0045d304ed71af5637b79209f43769058e
Eman Alaa Khodary <emanalaakhodary@gmail.com>
2024-03-12 02:02:54 +0200

    CORS angular

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -3,6 +3,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy(name: "angularlocalhost",
+					  policy =>
+					  {
+						  policy.AllowAnyHeader();
+						  //policy.AllowAnyOrigin();
+
+						  policy.WithOrigins("http://localhost:4200"); //angular origin 
+						  policy.AllowAnyMethod();
+					  });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,6 +29,7 @@ if (app.Environment.IsDevelopment())
 	app.UseSwaggerUI();
 }
 
+app.UseCors("angularlocalhost");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
