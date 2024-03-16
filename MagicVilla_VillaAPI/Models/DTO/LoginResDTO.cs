629035a514c8455d7f434685492e5b07a77ac232
Eman Alaa Khodary <emanalaakhodary@gmail.com>
2024-03-16 02:29:17 +0200

    edits

--- MagicVilla_VillaAPI/Models/DTO/LoginResDTO.cs
+++ MagicVilla_VillaAPI/Models/DTO/LoginResDTO.cs
@@ -2,7 +2,8 @@
 {
 	public class LoginResDTO
 	{
-		public localUser User { get; set; }
+		public UserDTO User { get; set; }
 		public string Token { get; set; }
+		public string Role { get; set; }
 	}
 }
