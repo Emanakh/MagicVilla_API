b632514a9798edcc3a16dccbfa15b0a526c107c9
Eman Alaa Khodary <emanalaakhodary@gmail.com>
2024-03-18 22:34:30 +0200

    Adding Auto mapper and DTOS

---
+++ MagicVilla_Web/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Models.DTO;
+
+namespace MagicVilla_Web
+{
+	public class MappingConfig : Profile
+	{
+		public MappingConfig()
+		{
+			CreateMap<VillaDTO, VillaCreatedDTO>().ReverseMap();
+			CreateMap<VillaDTO, VillaUpdatedDTO>().ReverseMap();
+
+
+			CreateMap<VillaNumberDTO, VillaNumberCreateDTO>().ReverseMap();
+			CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+
+		}
+	}
+}
